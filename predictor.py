d2ac6170ce0ed549b794ff9cee6cc2b7da4a6f09
NamanArora <naman.arora11@gmail.com>
2017-09-29 00:04:39 +0530

    Convert time to secs

--- predictor.py
+++ predictor.py
@@ -5,10 +5,15 @@ import os
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+from sklearn import linear_model
 
 ##Linear regression
 times=[]
 price=[]
+
+def predict_linear_regression(dates,prices,x):
+    
 
 def gen_path(sharename,base_dir='data'):
     path=os.path.join(base_dir,'{}.csv'.format(str(sharename)))
@@ -16,17 +21,19 @@ def gen_path(sharename,base_dir='data'):
     return path
 
 
+def convert_to_secs(time_d):
+    times = map(int, re.split(r"[:,]", time_d))
+    return times[0]*3600+ times[1]*60
+
 def get_data(sharename):
     path=gen_path(sharename)
     with open(path,"r") as file:
         csvreader = csv.reader(file)
         next(csvreader)
         for row in csvreader:
-            times.append(float(row[0]))
+            times.append(convert_to_secs(row[0]))
             price.append(float(row[1]))
 
 
 sharename = raw_input("Enter share name: ")
 get_data(sharename)
-print times
-print price
